66f4da945187bb416bbb800844bc1783c94d6631
Michael <michaelsoorys@gmail.com>
2023-08-03 15:20:35 +0300

    Init

--- apps/cometchat-bug-reproduction/src/app/app.routes.ts
+++ apps/cometchat-bug-reproduction/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'chat',
+    pathMatch: 'full',
+    loadChildren: () =>
+      import('@cometchat-bug-reproduction/chat').then((m) => m.ChatModule),
+  },
+];
